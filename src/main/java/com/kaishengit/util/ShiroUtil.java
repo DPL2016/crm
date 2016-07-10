9049d70f20fc878a815ed7d0210f5b3b206058a9
DPL2016 <dingpengliang@aliyun.com>
2016-07-10 10:42:05 +0800

    crm模板

---
+++ src/main/java/com/kaishengit/util/ShiroUtil.java
@@ -0,0 +1,16 @@
+package com.kaishengit.util;
+
+import org.apache.shiro.SecurityUtils;
+
+public class ShiroUtil {
+    public static User getCurrentUser(){
+        return (User) SecurityUtils.getSubject().getPrincipal();
+    }
+
+    public static Integer getCurrentUserID(){
+        return getCurrentUser().getId();
+    }
+    public static String getCurrentUserName(){
+        return getCurrentUser().getUsername();
+    }
+}
